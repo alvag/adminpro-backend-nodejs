bea6ce66c984e983bc303d38a556fc31094416b3
Max Alva <alva85@gmail.com>
2018-01-21 17:46:10 -0500

    deleted config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    SECRET_KEY: "miclavedetokens",
-    db: process.env.MONGODB || "mongodb://localhost:27017/hospitalDB",
-    port: process.env.PORT || 3000,
-    url: "http://localhost:3000/api",
-    GOOGLE_CLIENT_ID: "961872950765-f68d1nn2eleag3e61e9cb13hhol576h2.apps.googleusercontent.com",
-    GOOGLE_SECRET: "Srip0fBxUiiUrm_FHOimgXqm"
-};
